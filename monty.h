f17e9b5650d802bcfc4da851f66ec3f0c4379917
Dojinex <bitrusdauda9118@gmail.com.com>
2022-10-06 10:29:27 -0700

    task update commit

--- monty.h
+++ monty.h
@@ -1,11 +1,8 @@
-#ifndef TADEM_H
-#define TADEM_H
+#ifndef MONTY
+#define MONTY
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
 
@@ -16,7 +13,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  *
  */
 
@@ -33,7 +30,7 @@ struct stack_s *next;
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  *
  */
 
@@ -44,48 +41,62 @@ void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
+ * struct content  - opcode and its function
+ * @op_func: the opcode
+ * @value: function to handle the opcode
+ * @line_n: number of the line
  *
- * Description: carries values through the program
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
  *
  */
 
-typedef struct bus_s
+typedef struct content
 {
-char *arg;
-FILE *file;
-char *content;
-int lifi;
-}  bus_t;
-extern bus_t bus;
+char *op_func;
+char *value;
+int line_n;
+} content_v;
 
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
-int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
-void free_stack(stack_t *head);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-void f_queue(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
+/**
+ * struct stakit  - opcode and its function
+ * @int_value: idk.
+ * @data: idk.
+ * @fp: idk.
+ * @line: idk.
+ *
+ * Description: honestly, idk.
+ */
+
+typedef struct stakit
+{
+int int_value;
+content_v data;
+FILE *fp;
+char *line;
+} extern_var_t;
+extern extern_var_t ex;
+
+int main(int argc, char *argv[]);
+void read_file(FILE *fp);
+void execute_function(stack_t **stack);
+size_t not_empty(char *line);
+content_v clean_spaces(char *line);
+
+void monty_push(stack_t **top, unsigned int line_n);
+void monty_pall(stack_t **top, unsigned int line_n);
+void monty_pint(stack_t **top, unsigned int line_n);
+void monty_pop(stack_t **top, unsigned int line_n);
+void monty_swap(stack_t **top, unsigned int line_n);
+void monty_add(stack_t **top, unsigned int line_n);
+void monty_sub(stack_t **top_ptr, unsigned int line_n);
+void monty_mul(stack_t **top_ptr, unsigned int line_n);
+void monty_nop(stack_t **top_ptr, unsigned int line_n);
+void monty_div(stack_t **top_ptr, unsigned int line_n);
+void monty_mod(stack_t **top_ptr, unsigned int line_n);
+void monty_pchar(stack_t **top_ptr, unsigned int line_n);
+void monty_pstr(stack_t **top_ptr, unsigned int line_n);
+
+void free_stack(stack_t *top_ptr);
 
 #endif
